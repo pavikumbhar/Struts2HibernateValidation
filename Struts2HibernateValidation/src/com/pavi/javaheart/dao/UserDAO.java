81f0f04f13242a07e63570d1958d50f12fb8d25f
Pavi Kumbhar <pavikumbhar007@gmail.com>
2014-05-09 16:01:43 +0530

    Create UserDAO.java

---
+++ Struts2HibernateValidation/src/com/pavi/javaheart/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package com.pavi.javaheart.dao;
+
+import java.util.List;
+import com.pavi.javaheart.bean.User;
+
+public interface UserDAO {
+
+	public void saveOrUpdateUser(User user);
+	public List<User> listUser();
+	public User listUserById(Long userId);
+	public void deleteUser(Long userId);
+}
